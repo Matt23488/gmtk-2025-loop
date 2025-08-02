3234101edb2baf8a3b96dd70897cd29e6fdc91d5
Matthew Bryant <matt23488@gmail.com>
2025-08-02 13:00:50 -0400

    small thing

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,5 @@
 export function getImage(url: string, onload = () => {}): HTMLImageElement {
-    const img = document.createElement('img');
+    const img = new Image();
     img.src = url;
     img.onload = onload;
 
