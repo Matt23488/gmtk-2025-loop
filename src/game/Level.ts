523c5c837c8deffcec92dab632398ac8bcf86d17
Matthew Bryant <matt23488@gmail.com>
2025-08-02 20:38:07 -0400

    adjusted level boundary colors

--- src/game/Level.ts
+++ src/game/Level.ts
@@ -396,7 +396,7 @@ export default class Level {
     }
 
     #renderLoopBoundary(renderer: Renderer): void {
-        const color = this.#mobius ? 'rgba(255, 255, 255, 0.3)' : 'rgba(0, 0, 0, 0.7)';
+        const color = this.#mobius ? 'rgba(255, 0, 255, 0.1)' : 'rgba(0, 255, 0, 0.1)';
 
         renderer.renderLine(
             [
